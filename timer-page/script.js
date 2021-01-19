bb239d82d48e96b34724261887a0c6f6cb6bc29c
KrzysztofPhotos <75449838+KrzysztofPhotos@users.noreply.github.com>
2021-01-19 14:49:41 +0100

    Create script.js

---
+++ timer-page/script.js
@@ -0,0 +1,11 @@
+function clicks() {
+    // let number = document.querySelector("#number").value;
+    // let result = document.querySelector("#counter");
+
+    // result.innerHTML = "2";
+    alert("dsadsadsa");
+}
+
+function click() {
+    alert('clear2');
+  }
