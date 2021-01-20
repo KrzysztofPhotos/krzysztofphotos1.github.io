15922252bd18ff9daacf6146c3fe3eb3bc034453
KrzysztofPhotos <75449838+KrzysztofPhotos@users.noreply.github.com>
2021-01-20 09:26:12 +0100

    Update script.js

--- timer-page/script.js
+++ timer-page/script.js
@@ -1,11 +1,30 @@
-function clicks() {
-    // let number = document.querySelector("#number").value;
-    // let result = document.querySelector("#counter");
+function git() {
 
-    // result.innerHTML = "2";
-    alert("dsadsadsa");
+    let liczba = document.querySelector("#liczba-input").value;
+    //tutaj pobiera liczba i przypisuje do zmiennej 'liczba'
+
+    document.getElementById("start-button").disabled = true;
+    document.getElementById("liczba-input").disabled = true;
+    //disable button "GO" & input (number)
+
+    document.getElementById("counter").innerHTML = liczba;
+
+    // while (liczba > -1){
+        
+    //     let liczba = liczba - 1;
+
+        //here wait js
+
+    //     setTimeout(check, 1000);
+
+    //     document.getElementById("counter").innerHTML = liczba;
+    // }
+    
+    //document.getElementById("counter").innerHTML = liczba;
+    document.getElementById("counter").innerHTML = "tutaj";
+
+    document.getElementById("start-button").disabled = false;
+    document.getElementById("liczba-input").disabled = false;
+    // alert("IT WORKS");
 }
 
-function click() {
-    alert('clear2');
-  }
