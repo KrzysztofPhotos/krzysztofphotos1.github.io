187a2de28ba0c39427e159e1ae8206980b292ff7
KrzysztofPhotos <75449838+KrzysztofPhotos@users.noreply.github.com>
2022-02-07 13:01:10 +0100

    Update 8_buttons.ino

--- arduino/8_buttons.ino
+++ arduino/8_buttons.ino
@@ -3,49 +3,50 @@
 void setup()
    {
     Serial.begin(9600);
-    pinMode(2, OUTPUT);
+    pinMode(A0, INPUT);
+    
    }
 
 void loop() 
    {
     int a0=analogRead(A0);
     int val=a0;
-    if(a0==47)
+    if(a0>44 && a0<70)
      {
       //przycisk 1
       val=1;
      }
-    if(a0==85)
+    if(a0>89 && a0<115)
      {
       //przycisk 2
       val=2;
      }
-    if(a0==146)
+    if(a0>179 && a0<200)
      { 
       //przycisk 3
       val=3;
      }
-    if(a0==227)
+    if(a0>224 && a0<241)
      {
       //przycisk 4
       val=4;
      }
-    if(a0==268)
+    if(a0>259 && a0<272)
      {
       //przycisk 5
       val=5;
      }
-    if(a0==279)
+    if(a0>274 && a0<284)
      {
       //przycisk 6
       val=6;
      }
-    if(a0==298)
+    if(a0>293 && a0<301)
      {
       //przycisk 7
       val=7;
      }
-    if(a0==329) 
+    if(a0>319 && a0<336)
      {
       //przycisk 8
       val=8;
