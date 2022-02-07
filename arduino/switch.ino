c5e0a3c65fff9fd128959564efef700e2060a2ff
KrzysztofPhotos <75449838+KrzysztofPhotos@users.noreply.github.com>
2022-02-07 19:21:01 +0100

    Update switch.ino

--- arduino/switch.ino
+++ arduino/switch.ino
@@ -13,16 +13,12 @@ void setup(){
 void loop(){
   //ciagle sprawdza wartosc odczytu z przycisku (0 lub 1)
   btn_cons_1 = digitalRead(2);
-  
-  
 
   if(btn_cons_1 == 1 && var_temp == 0){
   Serial.println("Wartosc 1");
     var_temp = 1;
     delay(10);
   }
-  
-    
   
   if(btn_cons_1 == 0 && var_temp == 1){
   Serial.println("Wartosc 0");
