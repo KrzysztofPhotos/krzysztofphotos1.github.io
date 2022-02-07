561ef5007dc22822980a03d2a272df7614f2507d
KrzysztofPhotos <75449838+KrzysztofPhotos@users.noreply.github.com>
2022-02-07 17:48:54 +0100

    Update switch.ino

--- arduino/switch.ino
+++ arduino/switch.ino
@@ -4,7 +4,8 @@ int var_temp;
 void setup(){
   pinMode(2, INPUT_PULLUP);
   Serial.begin(9600);
-  //var_temp = digitalRead(2);
+  var_temp = digitalRead(2);
+  
 }
 
 
@@ -13,12 +14,25 @@ void loop(){
 
   if(btn_cons_1 == 1){
   Serial.println("Wartosc 1");
+    
+    if(var_temp == 0){
+      //przycisk zostal przelaczony
+      
+      int var_temp = 1;
+    }
+    
+    
   }
   else {
     Serial.println("Wartosc 0");
+    
+    if(var_temp == 1){
+      //przycisk zostal przelaczony
+      
+      int var_temp = 0;
+    }
+    
   }
   
-  
-  
-  
 }
+
