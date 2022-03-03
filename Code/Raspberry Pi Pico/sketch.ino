850d09c194b1b994f0d8e2b5dff9c006200a0c83
Krzychu <75449838+KrzysztofPhotos@users.noreply.github.com>
2022-03-03 19:58:34 +0100

    Update sketch.ino

--- Code/Raspberry Pi Pico/sketch.ino
+++ Code/Raspberry Pi Pico/sketch.ino
@@ -3,75 +3,43 @@
 
 PicoGamepad gamepad;
 
-// 16 bit integer for holding input values
-int val;
+int btn_cons_1;
+int btn_temp_1;
 
-int btn_circle_1;
-int btn_circle_1_temp;
-
-int temp3 = 0;
-int temp4 = 0;
-
-void setup() {
-  Serial.begin(115200);
-
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  // Button on pin
+void setup(){
   pinMode(0, INPUT_PULLUP);
-
-  btn_circle_1_temp = digitalRead(0);
+  Serial.begin(9600);
+  
+  //tutaj wartosc poczatkowa zostaje zadefiniowana
+  btn_temp_1 = digitalRead(0);
+  
 }
 
+void loop(){
+  //ciagle sprawdza wartosc odczytu z przycisku (0 lub 1)
+  btn_cons_1 = digitalRead(0);
 
-void loop() {
-
-  Serial.println(!digitalRead(0));
-
-  btn_circle_1 = digitalRead(0);
-
-  if (temp3 == 1) {
-    delay(500);
-    gamepad.SetButton(0, 0);
-    gamepad.send_update();  //chb nie wymagane sporbowac pozniej usunac
-    temp3 = 0;
-  }
-
-  if (temp4 == 1) {
-    delay(500);
-    gamepad.SetButton(1, 0);
-    gamepad.send_update();  //chb nie wymagane sporbowac pozniej usunac
-    temp4 = 0;
-  }
-
-  if (btn_circle_1 == 1 && btn_circle_1_temp == 0) {
-
-    gamepad.SetButton(1, 1);
+  if(btn_cons_1 == 1 && btn_temp_1 == 0){
+  Serial.println("Wartosc 1");
+    btn_temp_1 = 1;
     delay(10);
-    gamepad.send_update();
-    temp4 = 1;
-    btn_circle_1_temp = 0;
   }
-
-  if (btn_circle_1 == 0 && btn_circle_1_temp == 1) {
-    gamepad.SetButton(0, 1);
+  
+  if(btn_cons_1 == 0 && btn_temp_1 == 1){
+  Serial.println("Wartosc 0");
+    btn_temp_1 = 0;
     delay(10);
-    gamepad.send_update();
-    temp3 = 1;
-    btn_circle_1_temp = 0;
   }
 
 
-
-
-  // Send all inputs via HID
-  // Nothing is send to your computer until this is called.
   gamepad.send_update();
-
-  // Flash the LED just for fun
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   delay(300);
+  
 }
+
+
+
 
 //void btn1(){
 //gamepad.SetButton(0, 1);
