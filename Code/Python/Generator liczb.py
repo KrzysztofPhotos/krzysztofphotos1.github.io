a7c8cce89e12d045b8a63dfc0b11f1d8ad2ba5b2
Krzychu <75449838+KrzysztofPhotos@users.noreply.github.com>
2022-12-04 07:28:19 +0100

    Delete Code/Python directory

--- Code/Python/Generator liczb.py
+++
@@ -1,6 +0,0 @@
-print("Witaj w generatorze liczb!")
-start = input("Wpisz od której liczby ma zacząc generowac: ")
-end = input("Wpisz do ktorej liczby ma skończyć generować: ")
-
-for generate in range(int(start),int(end)+1):
-    print(generate)
